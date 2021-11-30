969742a92636c36ea232861c1748a4ace3406409
Amy Cox <amy_cox@me.com>
2021-11-30 13:55:33 +0000

    props rounds made a constant

--- src/ColourPicker.js
+++ src/ColourPicker.js
@@ -1,15 +1,23 @@
 import React from "react";
+import "./index.css";
 
-function generateColourPickerInputs(rounds) {
-    return
-}
+// function generateColourPickerInputs(rounds) {
+//     return
+// }
 
+// function Square(props) {
+//   return (
+//     <button className="square" onClick={props.onClick}>
+//       {props.value}
+//     </button>
+//   );
+// }
 export default function ColourPicker(props) {
-
+  const rounds = props.rounds;
   return (
     <>
       <h1>Colour Picker</h1>
-      <h2>{props.rounds}</h2>
+      <h2>{rounds}</h2>
     </>
   );
 }
